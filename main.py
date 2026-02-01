bc99e8296579da5466c05d24a1336411e705d1bb
Ducky1000-7 <165673518+Ducky1000-7@users.noreply.github.com>
2026-02-01 21:48:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,125 +63,114 @@ def create_block(x, y, output_path='block.png'):
 def make_room_walls(size_x, size_y):
     corner = Image.open('biom1wall2.png').resize((15 * 5, 15 * 5), 0)
     wall = Image.open('biom1wall1.png').resize((15 * 5, 15 * 5), 0)
-    empty111 = Image.open('biom1wall0.png').resize((15 * 5, 15 * 5), 0)
     exits = dictionary['exits']
+    tile_size = 15 * 5
 
-    result = Image.new('RGBA', (size_x * 15 * 5, size_y * 15 * 5), (0, 0, 0, 0))
+    wall_images = []
 
     for tile_row in range(size_y):
         for tile_col in range(size_x):
+            current_tile = None
 
+            # Логика определения, есть ли здесь стена
             if (tile_row == 0 and tile_col == 0) or (tile_row == size_y - 5 and tile_col == 0 and 3 in exits):
                 current_tile = corner.rotate(90)
-
             elif (tile_row == 0 and tile_col == size_x - 1) or (tile_row == size_y - 5 and tile_col == size_x - 1 and 4 in exits):
                 current_tile = corner
-
             elif (tile_row == size_y - 1 and tile_col == 0) or (tile_row == 4 and tile_col == 0 and 2 in exits):
                 current_tile = corner.rotate(180)
-
             elif (tile_row == size_y - 1 and tile_col == size_x - 1) or (tile_row == 4 and tile_col == size_x - 1 and 1 in exits):
                 current_tile = corner.rotate(270)
-
             elif tile_row == 0:
-                if 3 in exits and tile_row in [size_y - 2, size_y - 3, size_y - 4]:
-                    continue
-                current_tile = wall
+                if not (3 in exits and tile_row in [size_y - 2, size_y - 3, size_y - 4]):
+                    current_tile = wall
             elif tile_row == size_y - 1:
                 current_tile = wall.rotate(180)
             elif tile_col == 0:
-                if 2 in exits and tile_row in [1, 2, 3]:
-                    continue
-                elif 3 in exits and tile_row in [size_y - 2, size_y - 3, size_y - 4]:
-                    continue
-                current_tile = wall.rotate(90)
+                if not ((2 in exits and tile_row in [1, 2, 3]) or (3 in exits and tile_row in [size_y - 2, size_y - 3, size_y - 4])):
+                    current_tile = wall.rotate(90)
             elif tile_col == size_x - 1:
-                if 1 in exits and tile_row in [1, 2, 3]:
-                    continue
-                elif 4 in exits and tile_row in [size_y - 2, size_y - 3, size_y - 4]:
-                    continue
-                current_tile = wall.rotate(270)
+                if not ((1 in exits and tile_row in [1, 2, 3]) or (4 in exits and tile_row in [size_y - 2, size_y - 3, size_y - 4])):
+                    current_tile = wall.rotate(270)
 
-            else:
-                continue
+            if current_tile is not None:
+                x = tile_col * tile_size
+                y = tile_row * tile_size
+                wall_images.append((current_tile, x, y))
 
-            x_c = tile_col * 15 * 5
-            y_c = tile_row * 15 * 5
-
-            result.paste(current_tile, (x_c, y_c), current_tile)
-    return result
+    return wall_images
 
 
 class MyGame(arcade.Window):
     def __init__(self):
-        super().__init__(16 * 75, 9 * 75, "fimoz game")
-
-        room_img = make_room_walls(dictionary['shape'][0] // 5, dictionary['shape'][1] // 5)
-
-        img_bytes = io.BytesIO()
-        room_img.save(img_bytes, format='PNG')
-        img_bytes.seek(0)
-        texture = arcade.load_texture(img_bytes)
-
-        self.room_sprite = arcade.Sprite()
-        self.room_sprite.texture = texture
-        self.room_sprite.center_x = dictionary['shape'][0] * 5 * 1.5
-        self.room_sprite.center_y = dictionary['shape'][1] * 5 * 1.5
-
-        self.room_sprite_list = arcade.SpriteList()
-        self.room_sprite_list.append(self.room_sprite)
-        self.player_texture = arcade.load_texture(":resources:images/enemies/slimeBlue.png")
+        super().__init__(1200, 675, "fimoz game")
         self.camera = arcade.camera.Camera2D()
         self.setup()
 
     def setup(self):
+        size_x = dictionary['shape'][0] // 5  # 24
+        size_y = dictionary['shape'][1] // 5  # 20
+        wall_data = make_room_walls(size_x, size_y)
+
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+
+        for img, x, y in wall_data:
+            img_bytes = io.BytesIO()
+            img.save(img_bytes, format='PNG')
+            img_bytes.seek(0)
+            texture = arcade.load_texture(img_bytes)
+
+            wall_sprite = arcade.Sprite(texture)
+            wall_sprite.center_x = x + texture.width / 2
+            wall_sprite.center_y = y + texture.height / 2
+            self.wall_list.append(wall_sprite)
+
         self.player_sprite_list = arcade.SpriteList()
-        self.player = arcade.Sprite(self.player_texture, scale=2)
-        self.player.center_x = 300
-        self.player.center_y = 700
-        self.physics_engine = arcade.PhysicsEnginePlatformer(
-            self.player, self.room_sprite_list, gravity_constant=0.07)
+        self.player = arcade.Sprite(":resources:images/enemies/slimeBlue.png", scale=2)
+        self.player.center_x = size_x * 75 // 2
+        self.player.center_y = 200
         self.player_sprite_list.append(self.player)
 
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            player_sprite=self.player,
+            platforms=self.wall_list,
+            gravity_constant=0.5,
+            ladders=None,
+            walls=None
+        )
+
+        self.camera.viewport = (0, 0, self.width, self.height)
+        self.center_camera_to_player()
+
     def center_camera_to_player(self):
-        screen_center_x = self.player.center_x - (self.camera.viewport_width / 2)
-        screen_center_y = self.player.center_y - (self.camera.viewport_height / 2)
-
-        # Не двигаем камеру ниже нуля
-        screen_center_x = max(screen_center_x, 0)
-        screen_center_y = max(screen_center_y, 0)
-
-        player_centered = screen_center_x, screen_center_y
-        self.camera.position = player_centered
+        self.camera.position = (self.player.center_x, self.player.center_y)
 
     def on_draw(self):
-        self.clear()
-        self.camera.use()
-        self.room_sprite_list.draw()
+        self.clear(arcade.color.DARK_BLUE_GRAY)
+        self.wall_list.draw()
         self.player_sprite_list.draw()
+
 
     def on_update(self, delta_time: float):
         self.physics_engine.update()
         self.center_camera_to_player()
 
     def on_key_press(self, key, modifiers):
-        if key == arcade.key.UP:
-            self.player.change_y = SPEED
-        elif key == arcade.key.DOWN:
-            self.player.change_y = -SPEED
-        elif key == arcade.key.LEFT:
+        if key == arcade.key.LEFT:
             self.player.change_x = -SPEED
         elif key == arcade.key.RIGHT:
             self.player.change_x = SPEED
+        elif key == arcade.key.UP:
+            if self.physics_engine.can_jump():
+                self.player.change_y = 12
+        elif key == arcade.key.ESCAPE:
+            arcade.close_window()
 
     def on_key_release(self, key, modifiers):
-        if key in [arcade.key.UP, arcade.key.DOWN]:
-            self.player.change_y = 0
-        if key in [arcade.key.LEFT, arcade.key.RIGHT]:
+        if key in (arcade.key.LEFT, arcade.key.RIGHT):
             self.player.change_x = 0
 
 
 if __name__ == "__main__":
     game = MyGame()
-    arcade.run()
     arcade.run()
